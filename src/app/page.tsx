26978e13bf4f84edc956f89552f970f0e1dd01d9
Nauqh <hodominhquan.work@gmail.com>
2025-01-30 16:14:03 +0700

    fix(scroll to top): add overlay for search and menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,57 @@ import Link from "next/link";
 import Footer from "@/components/Footer";
 import mailgo from "mailgo";
 import postsData from "@/data/posts.json";
+import Image from "next/image";
 
 import { Fade } from "react-awesome-reveal";
 import { FaInstagram, FaLinkedin, FaRegEnvelope } from "react-icons/fa";
 
 import dynamic from "next/dynamic";
+
+const CollectionHover = () => {
+	return (
+		<div className="flex flex-col gap-4">
+			<div className="flex justify-start">
+				<div>
+					<h3 className="text-lg">
+						<Link
+							href="/posts/collection/sweet-cavity?tags=poetry,collection&img=https://static01.nyt.com/images/2022/03/01/arts/28flora1/28flora1-videoSixteenByNineJumbo1600.jpg"
+							className="hover:text-[#b39d90]"
+						>
+							Sweet Cavity
+						</Link>
+					</h3>
+					<Image
+						src="https://static01.nyt.com/images/2022/03/01/arts/28flora1/28flora1-videoSixteenByNineJumbo1600.jpg"
+						alt="Description"
+						width={150}
+						height={150}
+						className="mt-4 w-auto rounded-sm"
+					/>
+				</div>
+			</div>
+			<div className="flex justify-end">
+				<div>
+					<h3 className="text-lg">
+						<Link
+							href="/posts/collection/dream-journal?tags=fiction,short%20story,collection&img=https://images8.alphacoders.com/119/1197975.jpg"
+							className="hover:text-[#b39d90]"
+						>
+							Dream Journal
+						</Link>
+					</h3>
+					<Image
+						src="https://images8.alphacoders.com/119/1197975.jpg"
+						alt="Description"
+						width={150}
+						height={150}
+						className="mt-4 w-auto rounded-sm"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 const InstagramEmbed = dynamic(
 	() => import("react-social-media-embed").then((mod) => mod.InstagramEmbed),
@@ -26,9 +72,55 @@ const Page = () => {
 
 	return (
 		<div>
+			<div>
+				<ul
+					className="hidden lg:flex fixed top-1/2 flex-row gap-8 z-10 text-sm"
+					style={{ transform: "rotate(-90deg)" }}
+				>
+					{[
+						{
+							text: "collection",
+							href: "mailto:hodominhquan.self@gmail.com",
+							hasEmbed: false,
+						},
+						{
+							text: "social",
+							href: "https://www.instagram.com/kelramel_/",
+							hasEmbed: false,
+						},
+					].map((item, index) => (
+						<li key={index} className="relative group">
+							<Link
+								className="hover:text-[#b39d90] uppercase"
+								href={item.href}
+							>
+								{item.text}
+							</Link>
+
+							<div className="invisible opacity-0 group-hover:visible group-hover:opacity-100 absolute -right-40 transform rotate-90 w-[400px] h-[420px] rounded-sm -z-10 transition-all duration-300 ease-in-out">
+								<div className="absolute -left-[70px] top-[180px] w-20 h-10" />
+								{item.text === "collection" ? (
+									<div className="bg-[#faf6f1] p-8 h-full rounded-sm">
+										<h2 className="text-2xl uppercase">
+											{item.text}
+										</h2>
+										<hr className="my-2 border-gray-600" />
+										<CollectionHover />
+									</div>
+								) : (
+									<InstagramEmbed
+										url="https://www.instagram.com/kelramel_/"
+										width={328}
+									/>
+								)}
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
 			<Fade cascade={true} triggerOnce={true}>
 				<ul
-					className="hidden  fixed left-8 top-1/2 flex-col gap-2 z-10"
+					className="hidden fixed left-8 top-1/2 flex-col gap-2 z-10"
 					id="social"
 				>
 					<li>
@@ -46,35 +138,6 @@ const Page = () => {
 							<FaRegEnvelope className="text-2xl transition-all duration-300 hover:scale-110" />
 						</Link>
 					</li>
-				</ul>
-				<ul
-					className="hidden lg:flex fixed -left-10 top-1/2 flex-row gap-8 z-10 uppercase text-sm"
-					style={{ transform: "rotate(-90deg)" }}
-				>
-					{[
-						{
-							text: "email me",
-							href: "mailto:hodominhquan.self@gmail.com",
-							hasEmbed: false,
-						},
-						{ text: "feature", href: "/linkedin", hasEmbed: true },
-						{
-							text: "social",
-							href: "https://www.instagram.com/kelramel_/",
-							hasEmbed: true,
-						},
-					].map((item, index) => (
-						<li key={index} className="relative group">
-							<Link href={item.href}>{item.text}</Link>
-							{item.hasEmbed ? (
-								<div className="hidden group-hover:block absolute -right-20 transform rotate-90 w-[320px] h-[420px] z-20">
-									<InstagramEmbed url="https://www.instagram.com/kelramel_/" />
-								</div>
-							) : (
-								<div className="hidden group-hover:block absolute -right-20 transform rotate-90 w-[320px] h-[420px] z-20 bg-secondary" />
-							)}
-						</li>
-					))}
 				</ul>
 
 				<div className="container mx-auto px-4 pt-5" id="slogan">
@@ -106,7 +169,7 @@ const Page = () => {
 					</div>
 				</div>
 
-				<div className="container my-4 mx-auto px-4 py-8 bg-[#faf6f1] rounded-sm">
+				<div className="my-4 px-4 py-20 bg-[#faf6f1] rounded-sm">
 					<div className="max-w-md mx-auto text-center">
 						<h2 className="text-xl mb-4 uppercase">
 							Subscribe to Newsletter
